e3ed245e4f0da2fa408f292c5ba0d3ff01bd5066
arrase21 <arrase21@gmail.com>
2026-05-18 16:41:32 -0500

    add waybar in themes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,28 +27,67 @@ if status is-interactive
     alias pva 'source .env/bin/activate.fish'
     alias pga 'source pgadmin4/bin/activate.fish'
 end
-fish_add_path /home/arrase/.opencode/bin
 
+# FZF
+# source /usr/share/fzf/shell/key-bindings.fish
+# source /usr/share/fish/completions/fzf.fish
+#
+# function __fzf_files
+#     set -l dir (fd -H -t d . $HOME | fzf --preview 'eza --tree --level=2 --icons --color=always {} 2>/dev/null || tree -C {} | head -200')
+#     test -n "$dir"; and builtin cd "$dir"; and commandline -f repaint
+# end
+# bind \cf __fzf_files
+# set -Ux FZF_DEFAULT_OPTS '
+#     --height 70%
+#     --layout=reverse
+#     --border=rounded
+#     --prompt="❯ "
+#     --pointer="▶"
+#     --marker="✓"
+#     --color=fg:#c0caf5,bg:-1,hl:#B388FF 
+#     --color=fg+:#ffffff,bg+:-1,hl+:#B388FF 
+#     --color=info:#7aa2f7,prompt:#2CF9ED,pointer:#bb9af7 
+#     --color=marker:#9ece6a,spinner:#9ece6a,header:#565f89
+#     --popup=center
+#     --popup=70%
+# '
+#
 # FZF
 source /usr/share/fzf/shell/key-bindings.fish
 source /usr/share/fish/completions/fzf.fish
+
+set -Ux FZF_DEFAULT_OPTS "
+    --height 70%
+    --layout=reverse
+    --border=rounded
+    --prompt='❯ '
+    --pointer='▶'
+    --marker='✓'
+    --style=full
+    --padding=1,2
+    --border-label=' Demo ' 
+    --input-label=' Input '
+    --color=fg:#c0caf5,bg:-1,hl:#B388FF 
+    --color=fg+:#ffffff,bg+:-1,hl+:#B388FF 
+    --color=info:#7aa2f7,prompt:#2CF9ED,pointer:#bb9af7 
+    --color=marker:#9ece6a,spinner:#9ece6a,header:#565f89
+    --color='border:#aaaaaa,label:#cccccc' 
+    --color='preview-border:#9999cc,preview-label:#ccccff' 
+    --color='list-border:#669966,list-label:#99cc99' 
+    --color='input-border:#996666,input-label:#ffcccc'
+    --bind='result:transform-list-label:
+        if [ -z \"\$FZF_QUERY\" ]; then
+          echo \" \$FZF_MATCH_COUNT items \"
+        else
+          echo \" \$FZF_MATCH_COUNT matches for [\$FZF_QUERY] \"
+        fi
+        ' 
+    --bind='focus:transform-preview-label:[ -n {} ] && printf \" Previewing [%s] \" {}'
+"
 
 function __fzf_files
     set -l dir (fd -H -t d . $HOME | fzf --preview 'eza --tree --level=2 --icons --color=always {} 2>/dev/null || tree -C {} | head -200')
     test -n "$dir"; and builtin cd "$dir"; and commandline -f repaint
 end
+
 bind \cf __fzf_files
-set -Ux FZF_DEFAULT_OPTS '
-    --height 50%
-    --layout=reverse
-    --border=rounded
-    --prompt="❯ "
-    --pointer="▶"
-    --marker="✓"
-    --color=fg:#c0caf5,bg:-1,hl:#B388FF 
-    --color=fg+:#ffffff,bg+:-1,hl+:#B388FF 
-    --color=info:#7aa2f7,prompt:#2CF9ED,pointer:#bb9af7 
-    --color=marker:#9ece6a,spinner:#9ece6a,header:#565f89
-    --popup=center
-    --popup=70%
-'
